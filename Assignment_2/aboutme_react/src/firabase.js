a202e72dbf89d190a29e3edbe6cfbf16e84eada3
CertifiedForkliftDriver <124921581+CertifiedForkliftDriver@users.noreply.github.com>
2024-03-22 16:22:44 -0700

    succefsully added firebase

--- Assignment_2/aboutme_react/src/firabase.js
+++ Assignment_2/aboutme_react/src/firabase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firbase/database"
+import { getDatabase, ref, set, child, push, update } from "firebase/database"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,15 +9,20 @@ import { getDatabase } from "firbase/database"
 const firebaseConfig = {
   apiKey: "AIzaSyBFY06C-XKczoueFfzDjUgREzX2cj406ww",
   authDomain: "reactplatform-50568.firebaseapp.com",
+  databaseURL: "https://reactplatform-50568-default-rtdb.firebaseio.com",
   projectId: "reactplatform-50568",
   storageBucket: "reactplatform-50568.appspot.com",
   messagingSenderId: "690216822555",
   appId: "1:690216822555:web:bb66deef550031f29255be",
-  measurementId: "G-6W6H98L8WJ",
-  databaseURL: "https://console.firebase.google.com/u/0/project/reactplatform-50568/database/reactplatform-50568-default-rtdb/data/~2F"
+  measurementId: "G-6W6H98L8WJ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
+
+function writeToDB(test){
+  set(ref(database, 'users/'), {name: test});
+}
+writeToDB('ManyMen');
